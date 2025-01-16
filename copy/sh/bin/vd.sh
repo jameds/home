534fa2c6f74c5952aded2ad0601bd3c5beaabb40
James R <justsomejames2@gmail.com>
2025-01-16 13:23:24 -0800

    vd.sh: add options for fine-grained control

--- copy/sh/bin/vd.sh
+++ copy/sh/bin/vd.sh
@@ -1,9 +1,24 @@
 #!/bin/sh
+layout() {
+	swaymsg -q "input type:keyboard xkb_layout $1";
+}
 case "$1" in
 	1)
-		swaymsg -q 'input type:keyboard xkb_layout us'
+		otd applypreset Default
+		layout us
+		;;
+	'')
+		layout dpad
 		;;
 	*)
-		swaymsg -q 'input type:keyboard xkb_layout dpad'
-		;;
+	while getopts 'k:t:' o; do
+		case "$o" in
+			k)
+				layout "$OPTARG"
+				;;
+			t)
+				otd applypreset "$OPTARG"
+				;;
+		esac
+	done
 esac
