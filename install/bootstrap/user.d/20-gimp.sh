0d68d9f8e5b4f680d22dc69ae504de105ea24d1c
James R <justsomejames2@gmail.com>
2025-01-17 03:51:52 -0800

    install/bootstrap: update GIMP branch

--- install/bootstrap/user.d/20-gimp.sh
+++ install/bootstrap/user.d/20-gimp.sh
@@ -1,7 +1,7 @@
 bootstrap_gimp_dir="$(mktemp -d)"
 trap 'rm -rf "$bootstrap_gimp_dir"' EXIT
 git clone --depth 1 --recurse-submodules --shallow-submodules \
-	-b alt-color-picker https://github.com/jameds/gimp.git "$bootstrap_gimp_dir"
+	-b my-gimp https://github.com/jameds/gimp.git "$bootstrap_gimp_dir"
 cd "$bootstrap_gimp_dir"
 meson setup _build
 ninja -C _build
